66b770cfc3b7e80f961dc06d47c3e58b1aa2ebeb
fahmi54321 <fahmiabdulaziz44@gmail.com>
2021-05-02 22:50:46 +0700

    pulse animation

--- app/src/main/java/com/android/a30pulseanimation/MainActivity.java
+++ app/src/main/java/com/android/a30pulseanimation/MainActivity.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import pl.bclogic.pulsator4droid.library.PulsatorLayout;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        PulsatorLayout  pulsatorLayout = (PulsatorLayout) findViewById(R.id.pulsator);
+        pulsatorLayout.start();
+
     }
 }
